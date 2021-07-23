fdbc3d32b92e55997894681aff9ab257dfa2b523
Nur Ramadhan Jumali <nurramadhanjumali@yahoo.com>
2021-07-23 20:52:14 +0800

    Update

--- constants/mainMenuTabs.js
+++ constants/mainMenuTabs.js
@@ -1,28 +1,28 @@
 export default [
     {
       title: 'News',
-      image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+      image: 'https://image.freepik.com/free-vector/newspaper-with-croissant-cup-coffee-pencil_169241-3488.jpg',
     //   cta: 'View article', 
       horizontal: true
     },
     {
       title: 'Games',
-      image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+      image: 'https://thumbs.dreamstime.com/z/play-zone-banner-fun-game-playground-area-sign-balloon-pit-child-activity-park-advertisement-background-colorful-kids-190743826.jpg',
     //   cta: 'View article'
     },
     {
       title: 'Profile',
-      image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+      image: 'https://as2.ftcdn.net/v2/jpg/03/44/59/51/1000_F_344595103_gH2QPHuC8oc1buyzl3k0HtflxeJ12qxY.jpg',
     //   cta: 'View article' 
     },
     {
       title: 'Vicinity Sensor',
-      image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+      image: 'https://as1.ftcdn.net/v2/jpg/03/00/01/48/1000_F_300014849_X5RHdJDdsDxLJvOIx33qGTUspioo38IQ.jpg',
     //   cta: 'View article' 
     },
     {
       title: 'Contact Us',
-      image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+      image: 'https://as2.ftcdn.net/v2/jpg/00/87/60/87/500_F_87608744_bfya9OoOgV6roQKDgrdexmrzpmjxC9hd.jpg',
     //   cta: 'View article', 
       horizontal: true
     },

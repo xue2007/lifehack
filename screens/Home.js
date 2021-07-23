dc6c2c6310151ddf3e7d05a4acd5b11202b68150
xue2007 <chengweiliew@gmail.com>
2021-07-23 22:44:23 +0800

    local

--- screens/Home.js
+++ screens/Home.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { StyleSheet, Dimensions, ScrollView } from 'react-native';
-import { Block, theme } from 'galio-framework';
+import React from "react";
+import { StyleSheet, Dimensions, ScrollView } from "react-native";
+import { Block, theme } from "galio-framework";
 
-import { Card } from '../components';
-import mainMenuTabs from '../constants/mainMenuTabs';
-const { width } = Dimensions.get('screen');
+import { Card } from "../components";
+import mainMenuTabs from "../constants/mainMenuTabs";
+const { width } = Dimensions.get("screen");
 
 class Home extends React.Component {
   renderArticles = () => {
     return (
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.articles}>
+        contentContainerStyle={styles.articles}
+      >
         <Block flex>
-          
           <Block flex row>
-            <Card item={mainMenuTabs[0]} style={{ marginRight: theme.SIZES.BASE }} />
+            <Card
+              item={mainMenuTabs[0]}
+              style={{ marginRight: theme.SIZES.BASE }}
+            />
             <Card item={mainMenuTabs[1]} />
           </Block>
           <Block flex row>
-            <Card item={mainMenuTabs[2]} style={{ marginRight: theme.SIZES.BASE }} />
+            <Card
+              item={mainMenuTabs[2]}
+              style={{ marginRight: theme.SIZES.BASE }}
+            />
             <Card item={mainMenuTabs[3]} />
           </Block>
           <Block flex row>
             <Card item={mainMenuTabs[4]} />
           </Block>
-          
         </Block>
       </ScrollView>
-    )
-  }
+    );
+  };
 
   render() {
     return (
@@ -42,7 +47,7 @@ class Home extends React.Component {
 
 const styles = StyleSheet.create({
   home: {
-    width: width,    
+    width: width,
   },
   articles: {
     width: width - theme.SIZES.BASE * 2,
